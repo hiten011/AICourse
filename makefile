94e26a71c769c5d0d45596c96175381f59ba82c0
hiten <hitengupta12345@gmail.com>
2026-03-31 22:10:47 +1030

    parsing Text File

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	python pathfinder.py debug file.txt algo sa 
+	python pathfinder.py debug input.txt algo sa 
--- pathfinder.py
+++ pathfinder.py
@@ -1,39 +1,53 @@
-split = "/?"
-
 class PathFinder:
-    mode = ""
-    mapFile = ""
-    algo = ""
-    heuristic = ""
-
     # run simulation
     def run(self, cmds):
+        # 1. parse commands
         try:
             self.parseInput(cmds)
         except:
-            print("[Error] Parsing Failed")
+            print("[Error] Parsing CMDS Failed")
+            return False
+
+        # 2. parse input from text file
+        try:
+            self.parseMap(cmds)
+        except:
+            print("[Error] Parsing Text File Failed")
             return False
 
         return True
 
     # parsing commands
-    def parseInput(self, cmds):
-        parsed = cmds.split(split)
-
+    mode = ""
+    mapFile = ""
+    algo = ""
+    heuristic = ""
+    def parseInput(self, parsed):
         self.mode = parsed[0]
         self.mapFile = parsed[1]
         self.algo = parsed[2]
         self.heuristic = parsed[3]
 
+    # parsing input text file
+    adj = ""
+    r = ""
+    c = ""
+    st = ""
+    en = ""
+    def parseMap(self, parsed):
+        with open(self.mapFile, 'r') as file:
+            self.r, self.c = file.readline().split(" ")
+            self.st = file.readline().split(" ")
+            self.en = file.readline().split(" ")
+
+            self.adj = file.read()
+
 import sys
 def main():
     pf = PathFinder()
 
-    # parse commands
-    cmds = split.join(sys.argv[1:])
-
     # run simulation
-    if not pf.run(cmds):
+    if not pf.run(sys.argv[1:]):
         print("[EXIT] exiting...")
 
 if __name__ == "__main__":
