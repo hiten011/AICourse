46ae004ffae048976c613decec693ef1653e1bd6
hiten <hitengupta12345@gmail.com>
2026-05-28 20:00:24 +0930

    if gold found then then using bfs to navigate back

--- kb/knowledge_base.py
+++ kb/knowledge_base.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from .cell import Cell, CellStatus
-from .definitions import neighbors, action_toward
+from .definitions import neighbors
+from .navigator import action_toward, navigate_to
 
 
 class KnowledgeBase:
@@ -50,13 +51,17 @@ class KnowledgeBase:
     def mark_danger_wumpus(self, pos: tuple):
         self._map[pos].wumpus_score = 4        # evaluate_status auto-sets DANGER_WUMPUS
 
-    def next_action(self, pos: tuple, facing_idx: int) -> str:
+    def next_action(self, pos: tuple, facing_idx: int, map_size: tuple) -> str:
         if self.gold_grabbed and pos == (0, 0):
             return 'EXIT'
 
         if self._map[pos].has_glimmer and not self.gold_grabbed:
             self.gold_grabbed = True
             return 'GRAB'
+
+        if self.gold_grabbed:
+            action = navigate_to(self._map, pos, (0, 0), facing_idx, map_size)
+            return action if action is not None else 'NO_ACTION'
 
         r, c = pos
         neighbours = [
